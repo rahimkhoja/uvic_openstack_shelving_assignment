5d079bec834f0af6b021cc070bc5925d0985cadf
Rahim Khoja <13703075+rahimkhoja@users.noreply.github.com>
2024-09-15 22:11:14 -0700

    Create routes.py

---
+++ opt/uvic/shelving_web/routes/routes.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, abort, current_app, Response
+from flask_sitemap import Sitemap
+from models import sheldb, User, Subscription, MobileNumber, History, UserPreference, AssistantPreference 
+from utils.utility import *
+from datetime import datetime
+
+def configure_routes(app):
+
+    sitemap = Sitemap(app=app)
+
+    @app.context_processor
+    def inject_analytics():
+        return dict(
+            google_analytics_id=app.config['GOOGLE_ANALYTICS_ID'],
+            google_site_verification=app.config['GOOGLE_SITE_VERIFICATION'],
+            bing_site_verification=app.config['BING_SITE_VERIFICATION']
+        )
+
+    @app.after_request
+    def add_header(response):
+        # Ensure the endpoint is not None before checking its value
+        if request.endpoint and 'static' not in request.endpoint:
+            response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+            response.headers['Pragma'] = 'no-cache'
+            response.headers['Expires'] = '0'
+        return response
+    
+    @app.route('/robots.txt')
+    def robots_txt():
+        base_url = request.url_root[:-1]  # Remove the trailing slash
+        lines = [
+            "User-Agent: *",
+            "Allow: /",
+            f"Sitemap: {base_url}/sitemap.xml"
+        ]
+        return Response("\n".join(lines), mimetype="text/plain")
+
+
+    @app.route('/', methods=['GET', 'POST'])
+    def defer_page():
+        if session.get('user_provider_id'):
+            member = check_user_subscription(session.get('user_provider_id'))
+        else:
+            member = check_user_subscription(None)
+        current_app.logger.info('Info: Terms Page - Member Object: ' + str(member))
+        menu = generate_menu(member)
+        return render_template('terms.html', seometa=MetaTags, menu=menu)
+
+    
+    @sitemap.register_generator
+    def index():
+        yield 'index_page', {}
+
