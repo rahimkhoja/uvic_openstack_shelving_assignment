668ad4ff2b2d8c27d73d8ab4d3de3b94f2d35fcf
Rahim Khoja <13703075+rahimkhoja@users.noreply.github.com>
2024-09-15 21:59:11 -0700

    Update app.py

--- opt/uvic/shelving_web/app.py
+++ opt/uvic/shelving_web/app.py
@@ -1,1 +1,33 @@
-application
+from flask import Flask
+from config import Config
+from models import db 
+from utils.logger import setup_logger
+from routes.routes import configure_routes  
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+    app.config['SITEMAP_URL_SCHEME'] = 'https'
+    
+    # Initialize Extensions
+    db.init_app(app)
+
+    # Setup Logging
+    logger = setup_logger('app_logger',app.config['LOG_PATH'])
+    app.logger.addHandler(logger)
+    
+    # Configure Routes
+    configure_routes(app)
+
+    # Create tables if they do not exist
+    with app.app_context():
+        db.create_all()
+        
+    return app
+
+app = create_app()  # This line added
+
+if __name__ == '__main__':
+    app.run()
