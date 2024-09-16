7d0d523847324b73b8b07dbc35d9a8d8a4fec711
Rahim Khoja <13703075+rahimkhoja@users.noreply.github.com>
2024-09-15 21:41:24 -0700

    Update monday_shelve.py

--- opt/uvic/shelving_scripts/monday_shelve.py
+++ opt/uvic/shelving_scripts/monday_shelve.py
@@ -1,1 +1,11 @@
+import os
+import sqlite3
+import datetime
+from openstack import connection
 
+# Environment variables
+openstack_username = os.getenv('OPENSTACK_USERNAME')
+openstack_password = os.getenv('OPENSTACK_PASSWORD')
+openstack_domain = os.getenv('OPENSTACK_DOMAIN')
+research_it_email = os.getenv('RESEARCH_IT_EMAIL')
+sqlite_db_path = os.getenv('SQLITE_DB_PATH')
